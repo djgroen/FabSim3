78f01b86cd0116c1241d727819a1cd8b25cb51f1
Derek Groen <d.groen@ucl.ac.uk>
2015-12-07 11:55:53 +0000

    removed old reference to hg.

--- deploy/machines.py
+++ deploy/machines.py
@@ -149,8 +149,8 @@ def complete_environment():
 
     #env.build_number=subprocess.check_output(['hg','id','-q'.'-i']).strip()
     # check_output is 2.7 python and later only. Revert to oldfashioned popen.
-    cmd=os.popen(template("hg id -q -i"))
-    cmd.close()
+    #cmd=os.popen(template("hg id -q -i"))
+    #cmd.close()
     #env.build_number=run("hg id -q -i")
 
 complete_environment()
