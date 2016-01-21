3d68af8f4bf027dce50779bc017b8fef1628af83
Derek Groen <djgroennl@gmail.com>
2016-01-21 09:14:38 +0000

    Update fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,8 @@
 # 
 # This source file is part of the FabSim software toolkit, which is distributed under the BSD 3-Clause license. 
-# Please refer to LICENCE for detailed information regarding the licensing.
+# Please refer to LICENSE for detailed information regarding the licensing.
 #
-# fab.py is the main FabSim interface file. Here one can freely include or omit subsections of the FabSim toolkit,
+# fabfile.py is the main FabSim interface file. Here one can freely include or omit subsections of the FabSim toolkit,
 # to modify the enabled functionalities.
 
 #from deploy.fab import *
