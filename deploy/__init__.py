3decdbb600ef8e45a1789cd464eb30efeeb22fb1
Derek Groen <d.groen@ucl.ac.uk>
2018-05-22 09:42:04 +0100

    Updated disclaimers in __init__.py

--- deploy/__init__.py
+++ deploy/__init__.py
@@ -1,9 +1,5 @@
-# 
-# Copyright (C) University College London, 2007-2012, all rights reserved.
-# 
-# This file is part of HemeLB and is CONFIDENTIAL. You may not work 
-# with, install, use, duplicate, modify, redistribute or share this
-# file, or any part thereof, other than as allowed by any agreement
-# specifically made by you with University College London.
-# 
+#
+# This source file is part of the FabSim software toolkit, which is distributed under the BSD 3-Clause license.
+# Please refer to LICENSE for detailed information regarding the licensing.
+#
 
