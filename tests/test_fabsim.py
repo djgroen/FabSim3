4565509c346bb878de1ae79d34fb086e75c6a599
di73kuj2 <vytautas.jancauskas@lrz.de>
2018-09-06 16:47:41 +0200

    Move to pytest

---
+++ tests/test_fabsim.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + '/../base/')
+
+from deploy.templates import *
+from deploy.machines import *
+from fabric.contrib.project import *
+from base.fab import *
+
+import fileinput
+
+def test_fabsim():
+    assert("plugins" in get_plugin_path("FabDummy"))
+    assert("FabDummy" in get_plugin_path("FabDummy"))
+    assert(len(get_fabsim_git_hash()) > 0)
