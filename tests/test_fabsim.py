87816cd544f064441ceeb90e6b1d96dc1d16fd32
Robert Sinclair <rcsinclair3@gmail.com>
2018-10-17 17:20:06 +0100

    added fab_dummy tests

--- tests/test_fabsim.py
+++ tests/test_fabsim.py
@@ -1,7 +1,9 @@
 import os
 import sys
+import subprocess
 
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + '/../base/')
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + '../base/')
+#sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + '../../base/')
 
 from deploy.templates import *
 from deploy.machines import *
@@ -17,3 +19,8 @@ def test_fabsim():
     assert("plugins" in get_plugin_path("FabDummy"))
     assert("FabDummy" in get_plugin_path("FabDummy"))
     assert(len(get_fabsim_git_hash()) > 0)
+
+def test_fabdummy():
+    assert( subprocess.call(["fab", "localhost", "dummy:dummy_test"]) == 0)
+    assert( 'This dummy textfile has been printed with great success.' in subprocess.check_output(["fab", "localhost", "dummy:dummy_test"]).decode("utf-8"))
+    
