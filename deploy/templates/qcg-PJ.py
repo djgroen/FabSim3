e0f705b77762bc02587ede4edf5879564c77b627
Hamid <hamid.arabnejad@gmail.com>
2019-06-03 13:36:01 +0100

    updating template files

--- deploy/templates/qcg-PJ.py
+++ deploy/templates/qcg-PJ.py
@@ -1,21 +1,24 @@
-import zmq
-
 from qcg.appscheduler.api.manager import Manager
 from qcg.appscheduler.api.job import Jobs
 
-# switch on debugging (by default in api.log file)
 m = Manager(cfg={'log_level': 'DEBUG'})
+
 
 # get available resources
 print("available resources:\n%s\n" % str(m.resources()))
 
 
 # submit jobs and save their names in 'ids' list
-$submitted_jobs_list
-
-# list submited jobs
-print("submited jobs:\n%s\n" % str(m.list()))
+ids = m.submit(Jobs().$submitted_jobs_list
+               )
 
 
-# wait until all submited jobs finish
-# m.wait4all()
+# wait until submited jobs finish
+m.wait4(ids)
+
+# get detailed information about submited and finished jobs
+print("jobs details:\n%s\n" % str(m.info(ids)))
+
+# m.finish()
+# m.stopManager()
+# m.cleanup()
