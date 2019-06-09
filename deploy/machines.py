fef649528c79a9d2c498ad15567e86dcecc6bf8f
Hamid <hamid.arabnejad@gmail.com>
2019-06-09 20:06:46 +0100

    updating machines.py file

--- deploy/machines.py
+++ deploy/machines.py
@@ -3,7 +3,8 @@
 # This source file is part of the FabSim software toolkit, which is distributed under the BSD 3-Clause license.
 # Please refer to LICENSE for detailed information regarding the licensing.
 #
-# This file contains functions which help parse and process machine-specific definitions, written in machines*.yml
+# This file contains functions which help parse and process
+# machine-specific definitions, written in machines*.yml
 
 """
 Module defining how we configure the fabric environment for target machines.
@@ -33,60 +34,68 @@ import yaml
 from .templates import *
 from functools import *
 from pprint import PrettyPrinter
-pp=PrettyPrinter()
+pp = PrettyPrinter()
 
 
-
-#Root of local FabSim installation
+# Root of local FabSim installation
 env.localroot = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 env.localhome = os.path.expanduser("~")
-env.localplugins = {} #dict containing local paths to all plugins.
-env.no_ssh=False
-env.no_hg=False
-#Load and invoke the default non-machine specific config JSON dictionaries.
-config=yaml.load(open(os.path.join(env.localroot,'deploy','machines.yml')))
+env.localplugins = {}  # dict containing local paths to all plugins.
+env.no_ssh = False
+env.no_hg = False
+# Load and invoke the default non-machine specific config JSON dictionaries.
+config = yaml.load(open(os.path.join(env.localroot, 'deploy', 'machines.yml')))
 env.update(config['default'])
-user_config=yaml.load(open(os.path.join(env.localroot,'deploy','machines_user.yml')))
+user_config = yaml.load(
+    open(os.path.join(env.localroot, 'deploy', 'machines_user.yml')))
 env.update(user_config['default'])
-env.verbose=False
-env.needs_tarballs=False
-env.pather=posixpath
-env.remote=None
-env.node_type=None
-env.dollar="$"
-env.machine_name=None
-env.node_type_restriction=""
-env.node_type_restriction_template=""
+env.verbose = False
+env.needs_tarballs = False
+env.pather = posixpath
+env.remote = None
+env.node_type = None
+env.dollar = "$"
+env.machine_name = None
+env.node_type_restriction = ""
+env.node_type_restriction_template = ""
 # Maximum number of characters permitted in the name of a job in the queue system
 # -1 for unlimited
-env.max_job_name_chars=None
-env.lammps_exec="undefined"
-run_prefix_commands=env.run_prefix_commands[:]
+env.max_job_name_chars = None
+env.lammps_exec = "undefined"
+run_prefix_commands = env.run_prefix_commands[:]
 #run_prefix_commands.append("export PYTHONPATH=$$PYTHONPATH:$tools_build_path")
 if env.temp_path_template:
-  env.temp_path=template(env.temp_path_template)
+    env.temp_path = template(env.temp_path_template)
 #  run_prefix_commands.append(template("export TMP=$temp_path"))
 #  run_prefix_commands.append(template("export TMPDIR=$temp_path"))
 
-env.pythonroot=os.path.join(env.localroot,'python')
-env.blackboxroot=os.path.join(env.localroot,'blackbox')
+env.pythonroot = os.path.join(env.localroot, 'python')
+env.blackboxroot = os.path.join(env.localroot, 'blackbox')
 
 # job database configuration for remote machines
 env.local_jobsDB_path = os.path.join(env.localroot, 'deploy', '.jobsDB')
 env.local_jobsDB_filename = 'jobsDB.txt'
 
+
 def generate_module_commands(script=None):
-    module_commands = ["module %s"%module for module in env.modules["all"]] # Not using get as I want this to crash if the all key does not exist (it should always be present).
-    module_commands += ["module %s"%module for module in env.modules.get("nonexistent","")]
+    # Not using get as I want this to crash if the all key does not exist (it
+    # should always be present).
+    module_commands = ["module %s" % module for module in env.modules["all"]]
+    module_commands += ["module %s" %
+                        module for module in env.modules.get("nonexistent", "")]
 
     if script != None:
-        module_commands += ["module %s"%module for module in env.modules.get(script,"")] # Not using get as I want this to crash if the all key does not exist (it should always be present).
+        # Not using get as I want this to crash if the all key does not exist
+        # (it should always be present).
+        module_commands += ["module %s" %
+                            module for module in env.modules.get(script, "")]
         print("SCRIPT: ", script)
     #print("MODULE COMMANDS: ", module_commands)
     return module_commands
 
 module_commands = generate_module_commands()
-env.run_prefix=" && ".join(module_commands+list(map(template,run_prefix_commands))) or 'echo Running...'
+env.run_prefix = " && ".join(
+    module_commands + list(map(template, run_prefix_commands))) or 'echo Running...'
 
 
 @task
@@ -104,27 +113,30 @@ def machine(name):
     env.update(config[name])
     if name in user_config:
         env.update(user_config[name])
-    env.machine_name=name
+    env.machine_name = name
 
-    #Construct modules environment: update, not replace when overrides are done.
+    # Construct modules environment: update, not replace when overrides are
+    # done.
     env.modules = config["default"]["modules"]
     if "import" in config[name]:
         env.modules.update(config[config[name]["import"]].modules)
-    env.modules.update(config[name].get("modules",{}))
-    
+    env.modules.update(config[name].get("modules", {}))
+
     if "import" in config[name]:
         if config[name]["import"] in user_config:
             env.modules.update(user_config[config[name]["import"]].modules)
-        
-    env.modules.update(user_config[name].get("modules",{}))
+
+    env.modules.update(user_config[name].get("modules", {}))
 
     print(env.modules)
 
     complete_environment()
 
-#Metaprogram the machine wrappers
-for machine_name in set(config.keys())-set(['default']):
-    globals()[machine_name]=task(alias=machine_name)(partial(machine,machine_name))
+# Metaprogram the machine wrappers
+for machine_name in set(config.keys()) - set(['default']):
+    globals()[machine_name] = task(alias=machine_name)(
+        partial(machine, machine_name))
+
 
 def complete_environment():
     """Add paths to the environment based on information in the JSON configs.
@@ -144,36 +156,45 @@ def complete_environment():
     build_prefix: Command string to invoke before builds/installs are attempted
     build_number: Tip revision number of mercurial repository.
     """
-    env.hosts=['%s@%s'%(env.username,env.remote)]
-    env.host_string = '%s@%s'%(env.username,env.remote)
-    env.home_path=template(env.home_path_template)
-    env.runtime_path=template(env.runtime_path_template)
-    env.work_path=template(env.work_path_template)
-    env.remote_path=template(env.remote_path_template)
-    env.lammps_exec=template(env.lammps_exec)
-    env.lammps_args=template(env.lammps_args)
-    env.stat=template(env.stat)
-    env.results_path=env.pather.join(env.work_path,"results")
-    env.config_path=env.pather.join(env.work_path,"config_files")
-    env.scripts_path=env.pather.join(env.work_path,"scripts")
-    env.local_results=os.path.expanduser(template(env.local_results))
+    env.hosts = ['%s@%s' % (env.username, env.remote)]
+    env.host_string = '%s@%s' % (env.username, env.remote)
+    env.home_path = template(env.home_path_template)
+    env.runtime_path = template(env.runtime_path_template)
+    env.work_path = template(env.work_path_template)
+    env.remote_path = template(env.remote_path_template)
+    env.lammps_exec = template(env.lammps_exec)
+    env.lammps_args = template(env.lammps_args)
+    env.stat = template(env.stat)
+    env.results_path = env.pather.join(env.work_path, "results")
+    env.config_path = env.pather.join(env.work_path, "config_files")
+    env.scripts_path = env.pather.join(env.work_path, "scripts")
+    env.local_results = os.path.expanduser(template(env.local_results))
 
     for i in range(0, len(env.local_templates_path)):
-        env.local_templates_path[i]=os.path.expanduser(template(env.local_templates_path[i]))
+        env.local_templates_path[i] = os.path.expanduser(
+            template(env.local_templates_path[i]))
 
     for i in range(0, len(env.local_config_file_path)):
-        env.local_config_file_path[i]=os.path.expanduser(template(env.local_config_file_path[i]))
+        env.local_config_file_path[i] = os.path.expanduser(
+            template(env.local_config_file_path[i]))
 
-    module_commands = generate_module_commands(script = env.get("script",None))
-    env.run_prefix=" && ".join(module_commands+list(map(template,run_prefix_commands))) or 'echo Running...'
+    module_commands = generate_module_commands(script=env.get("script", None))
+    env.run_prefix = " && ".join(
+        module_commands + list(map(template, run_prefix_commands))) or 'echo Running...'
 
     if env.temp_path_template:
-        env.temp_path=template(env.temp_path_template)
+        env.temp_path = template(env.temp_path_template)
 
-    #env.build_number=subprocess.check_output(['hg','id','-q'.'-i']).strip()
+    if (hasattr(env, 'virtual_env_path') and env.virtual_env_path):
+        env.virtual_env_path = template(env.virtual_env_path)
+
+    if (hasattr(env, 'app_repository') and env.app_repository):
+        env.app_repository = template(env.app_repository)
+
+    # env.build_number=subprocess.check_output(['hg','id','-q'.'-i']).strip()
     # check_output is 2.7 python and later only. Revert to oldfashioned popen.
     #cmd=os.popen(template("hg id -q -i"))
-    #cmd.close()
+    # cmd.close()
     #env.build_number=run("hg id -q -i")
 
-#complete_environment()
+# complete_environment()
