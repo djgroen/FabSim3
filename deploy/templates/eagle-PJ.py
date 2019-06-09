52da2abe5aec5b63bd44360450980d26ab726b2c
Hamid <hamid.arabnejad@gmail.com>
2019-06-09 20:05:15 +0100

    add SLURM machine template

---
+++ deploy/templates/eagle-PJ.py
@@ -0,0 +1,24 @@
+from qcg.appscheduler.api.manager import LocalManager
+from qcg.appscheduler.api.job import Jobs
+
+m = LocalManager(cfg={'log_level': 'DEBUG'})
+
+
+# get available resources
+print("available resources:\n%s\n" % str(m.resources()))
+
+
+# submit jobs and save their names in 'ids' list
+ids = m.submit(Jobs().$submitted_jobs_list
+               )
+
+
+# wait until submited jobs finish
+m.wait4(ids)
+
+# get detailed information about submited and finished jobs
+print("jobs details:\n%s\n" % str(m.info(ids)))
+
+m.finish()
+m.stopManager()
+m.cleanup()
