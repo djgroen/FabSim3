4dea115c54dafcdf543452372d6d0afea1fa6802
Nicolas Monnier <nicolas.monnier44@gmail.com>
2019-10-22 15:09:49 +0200

    Add a synchronous function that wait all the job to be done

--- scalability/Async_thread_pool_job_management_prototype.py
+++ scalability/Async_thread_pool_job_management_prototype.py
@@ -8,7 +8,11 @@ from fabric.contrib.project import *
 from fabric.api import settings
 from fabric.operations import *
 
+
 class ClassTimeIt():
+    """
+        Class Timer, very useful to time some functions or more.
+    """
     def __init__(self, name=""):
         self.t0 = time.time()
         self.IsEnable = True
@@ -36,28 +40,65 @@ class ClassTimeIt():
 
 
 
-
+# Asynchronous Threads Pool Class
 class ATP:
     def __init__(self, ncpu=1):
         self.ncpu = ncpu
         self.job_executor = concurrent.futures.ThreadPoolExecutor(max_workers=self.ncpu)
         self.remote_jobs = {}
+        self.jobs_ID = {}
+        self.counter = 0
     
 
     def run_job(self, jobID, handler, counter=False, serial=False, args=(), **kwargs):
+        """
+        Put a job on a threading queue.
+        Args:
+            jobID   : string job identifier  
+            handler : function that will be executed by the job
+            counter : Counter that will be associated with a job and a jobID  ( Could be merged with jobID ...)
+            serial  : Serial mode (not implemented yet) 
+        """
         fn = self.job_executor.submit(handler, *args, **kwargs)
-        self.remote_jobs[jobID] = fn  
+        #self.remote_jobs[jobID] = fn
+        self.remote_jobs[self.counter] = fn
+        self.jobs_ID[self.counter] = jobID
+        self.counter += 1
 
     def awaitJobResult(self):
         jobs = []
         jobs_id = []
+        
+            
+    def awaitJobOver(self):
+        """
+        Wait for all the jobs to be done.            
+        """
+        #print([self.remote_jobs[jobID] for jobID in range(self.counter)])
+        concurrent.futures.wait([self.remote_jobs[jobID] for jobID in range(self.counter)], return_when=concurrent.futures.ALL_COMPLETED)
+        print("All threads are over")
+        
 
 
 def fake_sleep_job():
+    """
+    Small job that wait few second
+    """
     print("This is a fake sleep job")
     time.sleep(1)
 
 def rsync_job(remote_adress='', remote_path='', filepath='', filename=''):
+    """
+    Job that simulate FabSim3 behaviour.
+    Send files and execute few commands to the local and the remote machine.
+    Args:
+        remote_port     : ssh port of the remote machine (eg. 22)
+        remote_adress   : adress of the remote machine (eg. nicolasmonnier@adress.of.my.remote.fr)
+        remote_path     : result path of the remote machine (eg. /home/nicolas/FS3_scalability_result)
+        filepath        : local path of the file to send (eg. /home/nicolas/file_dir)
+        filename        : name of the local file to send (eg. sample_1)
+    """
+
     if not os.path.isfile(os.path.join(filepath, filename)):
         raise OSError('Error, file doesnt exist')
 
@@ -73,14 +114,14 @@ def rsync_job(remote_adress='', remote_path='', filepath='', filename=''):
 if __name__ == '__main__':
     print("Testing worker management protoype")
 
+    Timer = ClassTimeIt(name="Timer")
+
     nb_samples = 10
     current_path = '/home/nicolas/scalab_dev/FabSim3/scalability' 
     remote_result_path = '/home_nfs_robin_ib/bmonniern/utils/'
     remote_adress = 'bmonniern@castle.frec.bull.fr:'
    
-    local( "ls") 
 
-    #rsync_job(remote_adress = remote_adress, remote_path=remote_result_path, filepath=current_path, filename='sample_1')
     # Creation (if not exist) of the files to send to the remote 
     for sample in range(nb_samples):
         filename = 'sample_' + str(sample)
@@ -92,12 +133,18 @@ if __name__ == '__main__':
     
 
     # Creation of the Worker 
-    atp = ATP(ncpu=6)
-    for it in range(6):
-        print("Worker nb %d"%it)
-        atp.run_job(jobID=1, handler=fake_sleep_job)
+    # ncpu correspond to the number of simultaneous thread you want to set
+    atp = ATP(ncpu=2)
+
+    for sample in range(nb_samples):
+        filename = 'sample_' + str(sample)
+        atp.run_job(jobID=filename, handler=rsync_job, args=(remote_adress, remote_result_path, current_path, filename))
 
 
     print("The jobs are running")
+    atp.awaitJobOver() # Wait for all jobs to be done
+    
+    Timer.timeit()
+    
 
 #rsync ./cycler-0.10.0.tar.gz  bmonniern@castle.frec.bull.fr:9922:/home_nfs_robin_ib/bmonniern/utils/
