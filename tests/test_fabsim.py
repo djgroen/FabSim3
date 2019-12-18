bc9aee598ce62bdff4bb39daeb9bdb4622140fa8
Hamid Arabnejad <hamid.arabnejad@gmail.com>
2019-12-18 16:25:37 +0000

    add nb_thread

--- tests/test_fabsim.py
+++ tests/test_fabsim.py
@@ -55,9 +55,9 @@ def test_fabdummy_with_manual_ssh():
 
 def test_dummy_fabdummy_replicas_without_manual_ssh():
     assert(subprocess.call(
-        ["fab", "localhost", "dummy:dummy_test,replicas=5,manual_ssh=false"]) == 0)
+        ["fab", "localhost", "dummy:dummy_test,replicas=5,nb_thread=2,manual_ssh=false"]) == 0)
     output = subprocess.check_output(
-        ["fab", "localhost", "dummy:dummy_test,replicas=5,manual_ssh=false"]).decode("utf-8")
+        ["fab", "localhost", "dummy:dummy_test,replicas=5,nb_thread=2,manual_ssh=false"]).decode("utf-8")
     assert(output.find('success') >= 0)
 
 
