9f6fab319a6f880757d41437fc41b491efe296c1
Hamid <hamid.arabnejad@gmail.com>
2020-05-17 12:27:07 +0100

    build docs with readthedocs extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,6 +46,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx_tabs.tabs',
+    'readthedocs_ext.readthedocs',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
