12d6d1f00b1c21c058253bbde5232439f287098c
Arindam Saha <arindamsaha1507@gmail.com>
2023-07-25 13:40:21 +0100

    Improved user interface

--- space_remover.sh
+++ space_remover.sh
@@ -1,22 +1,17 @@
-function fabsim_modified_execution() {
+function fabsim_autocomplete() {
     local cmd="$*"
     local modified_cmd=""
     local arr=""
 
-    # Check if the command starts with "fabsim"
-    if [[ "$cmd" == "fabsim"* ]]; then
-        # Modify the command to preserve only the first two spacesa
+    # Modify the command to preserve only the first two spacesa
 
-        segment=$(echo "$cmd" | cut -d ' ' -f 3-100)
-        machine=$(echo "$cmd" | cut -d ' ' -f 2)
+    segment=$(echo "$cmd" | cut -d ' ' -f 2-100)
+    machine=$(echo "$cmd" | cut -d ' ' -f 1)
 
-        modified_cmd=$(echo "$segment" | sed 's/\(  *\)//g')
+    modified_cmd=$(echo "$segment" | sed 's/\(  *\)//g')
 
-        # Execute the modified command
-        eval "fabsim $machine $modified_cmd"
-    else
-        # Execute the original command as it is
-        eval "$cmd"
-    fi
+    # Execute the modified command
+    eval "fabsim $machine $modified_cmd"
 }
+
 
