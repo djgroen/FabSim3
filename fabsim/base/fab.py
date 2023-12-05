0b68670017ecc83357a40cac14426733fd9276e9
Derek Groen <djgroennl@gmail.com>
2023-12-05 15:09:37 +0000

    Pep8

--- fabsim/base/fab.py
+++ fabsim/base/fab.py
@@ -1056,7 +1056,7 @@ def job_submission(*job_args):
 @task
 @beartype
 def ensemble2campaign(
-    results_dir: str, campaign_dir: str, skip: Optional[Union[int, str]]=0
+    results_dir: str, campaign_dir: str, skip: Optional[Union[int, str]] = 0
 ) -> None:
     """
     Converts FabSim3 ensemble results to EasyVVUQ campaign definition.
@@ -1090,7 +1090,7 @@ def ensemble2campaign(
 @task
 @beartype
 def campaign2ensemble(
-    config: str, campaign_dir: str, skip: Optional[Union[int, str]]=0
+    config: str, campaign_dir: str, skip: Optional[Union[int, str]] = 0
 ) -> None:
     """
     Converts an EasyVVUQ campaign run set TO a FabSim3 ensemble definition.
